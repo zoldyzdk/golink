5e9d5571f64661348b5f60251f935db92afc32a7
Hewerton Zoldy <hewerton.soares@yahoo.com>
2025-03-04 22:17:29 -0300

    feat(api): configuring main api with chi router

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome!"))
+	})
+	http.ListenAndServe(":3000", r)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zoldyzdk/golink
+
+go 1.24.0
+
+require github.com/go-chi/chi/v5 v5.2.1
